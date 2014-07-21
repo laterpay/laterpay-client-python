c0ef54af20edb0bf45b3cc14ed3ae9a0a0ceb3c8
Kristian Glass <git@doismellburning.co.uk>
2014-07-21 15:41:29 +0100

    Fail cleanly when README isn't in .

--- setup.py
+++ setup.py
@@ -3,18 +3,24 @@ from distutils.core import setup
 from setuptools import find_packages
 
 import codecs
+import os
 
 #import time
 #_version = "3.0.dev%s" % int(time.time())
 _version = "3.0.0"
 _packages = find_packages('.', exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
 
+if os.path.exists('README.rst'):
+    _long_description = codecs.open('README.rst', 'r', 'utf-8').read()
+else:
+    _long_description = ""
+
 setup(
     name='laterpay-client',
     version=_version,
 
     description="LaterPay API client",
-    long_description=codecs.open("README.rst", encoding='utf-8').read(),
+    long_description=_long_description,
     author="LaterPay GmbH",
     author_email="support@laterpay.net",
     url="https://github.com/laterpay/laterpay-client-python",
